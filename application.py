39d8afd2ccad3a36a0b69ebd8bb34397960f9923
Julian <julian.builes.external@enel.com>
2023-02-05 19:34:33 -0500

    priemer commit

--- application.py
+++ application.py
@@ -1,8 +1,9 @@
 from flask import Flask, make_response, jsonify
+import models
 application = Flask(__name__)
 # print a nice greeting.
 @application.route("/",methods=["GET"])
-def say_hello(username = "World"):
+def say_hello():
     return make_response(jsonify({"hola":"hola"}))
 
 
