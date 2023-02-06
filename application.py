3a1b9b84219b64bdee0a99d885fe5cad500eb70d
Julian <julian.builes.external@enel.com>
2023-02-05 19:02:31 -0500

    priemer commit

--- application.py
+++ application.py
@@ -1,24 +1,12 @@
-__version__ = '1.0'
-__author__ = 'Julian Camilo Builes Serrano'
-
-__version__ = '1.0'
-__author__ = 'Julian Camilo Builes Serrano'
-
-from flask import Flask, make_response, jsonify, request
-from flask_cors import CORS
+from flask import Flask, make_response, jsonify
+application = Flask(__name__)
+# print a nice greeting.
+@application.route("/",methods=["GET"])
+def say_hello(username = "World"):
+    return make_response(jsonify({"hola":"hola"}))
 
 
-app = Flask(__name__)
-
-app.config[
-    "SECRET_KEY"
-] = "1dafafghsdsf5378167uevsbg423)(dfghj98797781234741arfcshzgwffzgnssaerASXMHMRMDwefsrvs8945)(/%#"
-app.secret_key = "test_secret"
-
-@app.route("/", methods=["GET"])
-def main():
-    return make_response(jsonify(["hola"]))
-
-
+# run the app.
 if __name__ == "__main__":
-    app.run()
+
+    application.run()
